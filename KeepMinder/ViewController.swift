831392c851b01ecc2e572772d48bb6a9c4bbbd91
Stella Hu <xinghu0164@gmail.com>
2018-03-23 20:52:46 +1100

    init project

---
+++ KeepMinder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeepMinder
+//
+//  Created by Xing Hu on 23/3/18.
+//  Copyright © 2018 Stella Hu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
