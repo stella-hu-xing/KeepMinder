c2b6a4e4ba52eafd93273f6122c92db0e1b5a671
Stella Hu <xinghu0164@gmail.com>
2018-04-09 22:13:09 +1000

    remove ViewController and add function: tap out of window to close the window

--- KeepMinder/AppDelegate.swift
+++ KeepMinder/AppDelegate.swift
@@ -16,39 +16,57 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     //Status Item — application icon — in the menu bar with a fixed length that the user will see and use.
     let firststatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
     let SecondestatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
     let popOver = NSPopover()
+    
+    var eventMonitor: EventMonitor?
+
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-
         let button = firststatusItem.button!
         button.image = NSImage(named: NSImage.Name("StatusBarButtonImage"))
         button.action = #selector(togglePopover(_:))
         
-        let item = SecondestatusItem.button!
-//        item.title = "这里显示一条提醒事项内容"
+
         popOver.contentViewController = ReminderViewController.freshController()
+        
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]){
+            [weak self] event in 
+            if let strongSelf = self, strongSelf.popOver.isShown{
+                strongSelf.closePopover(sender: event)
+            }
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         
     }
-
+    
+    
     @objc func togglePopover(_ sender: Any?) {
+        let item = SecondestatusItem.button!
         if popOver.isShown {
             closePopover(sender: sender)
+            
+            item.title = "这里显示一条提醒事项内容"
         } else {
             showPopover(sender: sender)
+             item.title = ""
         }
     }
     
     func showPopover(sender: Any?) {
         if let button = firststatusItem.button {
             popOver.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            eventMonitor?.start()
         }
     }
     
     func closePopover(sender: Any?) {
         popOver.performClose(sender)
+        eventMonitor?.stop()
     }
+
+
 }
 
---
+++ KeepMinder/EventMonitor.swift
@@ -0,0 +1,36 @@
+//
+//  EventMonitor.swift
+//  KeepMinder
+//
+//  Created by Xing Hu on 9/4/18.
+//  Copyright © 2018 Stella Hu. All rights reserved.
+//
+
+import Cocoa
+
+public class EventMonitor {
+    private var monitor: Any?
+    private let mask: NSEvent.EventTypeMask
+    private let handler: (NSEvent?) -> Void
+    
+    public init(mask: NSEvent.EventTypeMask, handler: @escaping (NSEvent?) -> Void) {
+        self.mask = mask
+        self.handler = handler
+    }
+    
+    deinit {
+        stop()
+    }
+    
+    public func start() {
+        monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler)
+    }
+    
+    public func stop() {
+        if monitor != nil {
+            NSEvent.removeMonitor(monitor!)
+            monitor = nil
+        }
+    }
+}
+
